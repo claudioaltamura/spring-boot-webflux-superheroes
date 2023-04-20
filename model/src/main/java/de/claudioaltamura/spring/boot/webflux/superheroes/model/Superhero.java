17f44d4881b93180aa37a4f77b253b1939d4826a
Claudio Altamura <claudio@claudioaltamura.de>
2023-04-20 17:46:59 +0200

    feat(model): provided toString

--- model/src/main/java/de/claudioaltamura/spring/boot/webflux/superheroes/model/Superhero.java
+++ model/src/main/java/de/claudioaltamura/spring/boot/webflux/superheroes/model/Superhero.java
@@ -3,10 +3,12 @@ package de.claudioaltamura.spring.boot.webflux.superheroes.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class Superhero {
     private  Long id;
     private String name;
