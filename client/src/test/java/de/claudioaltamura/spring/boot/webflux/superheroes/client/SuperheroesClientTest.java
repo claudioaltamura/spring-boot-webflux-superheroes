c9267a239a9fb88b4acceaddc496d5ff0aa68061
Claudio Altamura <claudio@claudioaltamura.de>
2023-04-21 10:45:19 +0200

    chore(server): formatting

---
+++ client/src/test/java/de/claudioaltamura/spring/boot/webflux/superheroes/client/SuperheroesClientTest.java
@@ -0,0 +1,54 @@
+package de.claudioaltamura.spring.boot.webflux.superheroes.client;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SuperheroesClientTest {
+    private static MockWebServer mockWebServer;
+    private SuperheroesClient cut; // Class Under Test
+
+    @BeforeAll
+    static void setup() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+    }
+
+    @AfterAll
+    static void tearDown() throws IOException {
+        mockWebServer.shutdown();
+    }
+
+    @Test
+    public void testGetSuperheroById() throws IOException, InterruptedException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+
+        this.cut = new SuperheroesClient(WebClient
+                .builder()
+                .baseUrl(mockWebServer.url("/").toString())
+                .build());
+
+        var mockResponse = new MockResponse()
+                .addHeader("Content-Type", "application/json; charset=utf-8")
+                .setBody("{\"id\": 1, \"name\":\"A\"}");
+
+        mockWebServer.enqueue(mockResponse);
+
+        var result = cut.getSuperheroBlocking(1);
+
+        assertEquals(1, result.getId());
+        assertEquals("A", result.getName());
+
+        RecordedRequest request = mockWebServer.takeRequest();
+        assertEquals("/superheroes/1", request.getPath());
+    }
+}
