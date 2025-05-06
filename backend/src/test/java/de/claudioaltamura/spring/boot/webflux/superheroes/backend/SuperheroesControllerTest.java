cee239a5883935fae74c26a73a401d9f8c5a785f
claudioaltamura <claudio@claudioaltamura.de>
2025-05-06 18:14:22 +0200

    test(SuperheroesController): mocking service

--- backend/src/test/java/de/claudioaltamura/spring/boot/webflux/superheroes/backend/SuperheroesControllerTest.java
+++ backend/src/test/java/de/claudioaltamura/spring/boot/webflux/superheroes/backend/SuperheroesControllerTest.java
@@ -1,48 +1,64 @@
 package de.claudioaltamura.spring.boot.webflux.superheroes.backend;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 import de.claudioaltamura.spring.boot.webflux.superheroes.model.Superhero;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 class SuperheroesControllerTest {
 
-  @Autowired private WebTestClient webTestClient;
+    WebTestClient webTestClient;
 
-  @Test
-  void shouldReturnSuperhero() {
-    webTestClient
-        .get()
-        .uri(uriBuilder -> uriBuilder.path("/superheroes/{id}").build(2))
-        .accept(MediaType.APPLICATION_JSON)
-        .exchange()
-        .expectStatus()
-        .isOk()
-        .expectBody(Superhero.class)
-        .value(superhero -> assertThat(superhero.getName()).isNotNull());
-  }
+    SuperheroesService superheroesService;
 
-  @Test
-  void shouldReturnSuperheroes() {
-    webTestClient
-        .get()
-        .uri("/superheroes")
-        .accept(MediaType.APPLICATION_JSON)
-        .exchange()
-        .expectStatus()
-        .isOk()
-        .expectBodyList(Superhero.class)
-        .hasSize(2)
-        .consumeWith(
-            response -> {
-              var superheroes = response.getResponseBody();
-              assertThat(superheroes).isNotNull();
-              System.out.println(superheroes);
-            });
-  }
+    @BeforeEach
+    void setUp() {
+        superheroesService = mock(SuperheroesService.class);
+        webTestClient = WebTestClient.bindToController(new SuperheroesController(superheroesService)).build();
+    }
+
+    @Test
+    void shouldReturnSuperhero() {
+        when(superheroesService.getSuperhero(2L))
+                .thenReturn(Mono.just(new Superhero(2L, "Spiderman")));
+
+        webTestClient
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/superheroes/{id}").build(2))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(Superhero.class)
+                .value(superhero -> assertThat(superhero.getName()).isNotNull());
+    }
+
+    @Test
+    void shouldReturnSuperheroes() {
+        when(superheroesService.getSuperheroes())
+                .thenReturn(Flux.just(new Superhero(1L, "Hulk"), new Superhero(2L, "Spiderman")));
+
+        webTestClient
+                .get()
+                .uri("/superheroes")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(Superhero.class)
+                .hasSize(2)
+                .consumeWith(
+                        response -> {
+                            var superheroes = response.getResponseBody();
+                            assertThat(superheroes).isNotNull();
+                            System.out.println(superheroes);
+                        });
+    }
 }
